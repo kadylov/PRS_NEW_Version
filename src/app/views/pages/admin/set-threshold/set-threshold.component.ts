93660b12430dc9ee6c627e595d7d93683d774aa9
Kamil Adylov <kradylov@gmail.com>
2020-05-13 17:36:05 -0600

    added notification for setting threshold

--- src/app/views/pages/admin/set-threshold/set-threshold.component.ts
+++ src/app/views/pages/admin/set-threshold/set-threshold.component.ts
@@ -1,76 +1,79 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import {AdminService} from '../../../../core/admin/_services/admin.service';
 import {Location} from '@angular/common';
+import {Subscription} from 'rxjs';
+import {LayoutUtilsService, MessageType} from '../../../../core/_base/crud';
 
 @Component({
 	selector: 'kt-set-threshold',
 	templateUrl: './set-threshold.component.html',
 	styles: [`
-		.mat-slider {
-			width: 75%;
-		}
-
-		.example-h2 {
-			margin: 10px;
-		}
-
-		.example-section {
-			display: flex;
-			align-content: center;
-			align-items: center;
-			height: 60px;
-		}
-
 		.example-margin {
 			margin: 10px;
 		}
-
-		.mat-slider-horizontal {
-			width: 300px;
-		}
-
-		.mat-slider-vertical {
-			height: 300px;
-		}
-	`]
+	`],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SetThresholdComponent implements OnInit, OnDestroy {
 
-	value: number = 55;			// value for setting threshold
+	// value for setting threshold
+	value: number = 55;
 	thresholdControl = new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/), Validators.min(1)]);
 
+	subscription:Subscription[]=[];
+
 	constructor(private location: Location,
-				private adminService: AdminService) {
+				private ref: ChangeDetectorRef,
+				private layoutUtilsService: LayoutUtilsService,
+				private adminService: AdminService
+
+	) {
 	}
 
 	ngOnInit() {
+
+		// load the value for the threshold from the server
+		// and assign it to the variable with name 'value'
+		this.subscription.push(this.adminService.getThreshold().subscribe(res=>{
+			if (res) {
+				this.value= res;
+				this.ref.markForCheck();
+			}
+		}));
 	}
 
 	ngOnDestroy(): void {
+		if (this.subscription.length>0) {
+			this.subscription.forEach(sub=>{sub.unsubscribe()});
+		}
 	}
 
 	onSubmit(b: boolean) {
 
-		// check from control
+		// validate user input for the threshold
 		if (this.thresholdControl.invalid) {
 			this.thresholdControl.markAllAsTouched();
 			return;
 		}
 
-		console.log(this.value);
 
-		this.adminService.sendThreshold(this.value).subscribe(
+
+
+		// submit new value for the threshold to the server
+		// display a confirmation message if there is no error from the server
+		this.subscription.push(this.adminService.sendThreshold(this.value).subscribe(
 			() => {
+				const message = `Threshold has been updated successfully`;
+				this.layoutUtilsService.showActionNotification(message, MessageType.Update, 4000, false, false);
+
 				this.location.back();
 			},
 
 			error => {
 				console.log(error);
 			}
-		);
-
-
+		));
 	}
 
 	isControlHasError(validationType: string): boolean {
